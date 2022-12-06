bf356bbdc64a570359623e7a219c2a3dc3cd275a
lessy2014 <lessik03@gmail.com>
2022-12-06 12:37:15 +0500

    as string fix

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -18,7 +18,7 @@ export class AuthenticateMiddleware implements MiddlewareInterface {
 
     try {
       const {payload} = await jose.jwtVerify(token, createSecretKey(this.jwtSecret, 'utf-8'));
-      req.user = { email: payload.email as string, id: payload.id as string };
+      req.user = { email: String(payload.email), id: String(payload.id) };
 
       return next();
     } catch {
